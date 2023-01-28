2b9d02db18b2b3bec5c74fe71f078b4b7c6d9c0e
AAKVAS <aleksejplotniklv@gmail.com>
2023-01-28 16:01:00 +0300

    Требования расширил, бд скрипт обновил

--- sql/вспомогательные запросы/insert_order_history.sql
+++ sql/вспомогательные запросы/insert_order_history.sql
@@ -2,16 +2,11 @@ DECLARE @DATE datetime;
 DECLARE @ORDER_ID int;
 DECLARE @CURRENT_STORAGE_ID int;
 DECLARE @FINISH_POINT int;
-SET @DATE = DATEADD(HOUR, RAND(CHECKSUM(NEWID())) * (24),
-				DATEADD(MINUTE, RAND(CHECKSUM(NEWID())) * (60),
-					DATEADD(SECOND, RAND(CHECKSUM(NEWID())) * (60),
-						DATEADD(MONTH, RAND(CHECKSUM(NEWID())) * (3), Cast('7/7/2022' as datetime))
-					)
-				)
-			);
-SET @ORDER_ID = 14;
+
+SET @ORDER_ID = 20;
+SET @DATE = (SELECT created_at FROM orders WHERE id = @ORDER_ID);
 SET @CURRENT_STORAGE_ID = 3;
-SET @FINISH_POINT = 13;
+SET @FINISH_POINT = (SELECT pick_up_point_id FROM orders WHERE id = @ORDER_ID);
 
 INSERT INTO order_history (order_id, status_changed_at, status_id, current_storage_id)
 SELECT @ORDER_ID, @DATE, 1, @CURRENT_STORAGE_ID;
@@ -39,8 +34,8 @@ SET @DATE = DATEADD(DAY, RAND(CHECKSUM(NEWID())) * (3),
 					)
 				)
 			);
-			/*
-SET @CURRENT_STORAGE_ID = 2;
+			
+SET @CURRENT_STORAGE_ID = 1;
 
 INSERT INTO order_history (order_id, status_changed_at, status_id, current_storage_id)
 SELECT @ORDER_ID, @DATE, 7, @CURRENT_STORAGE_ID;
@@ -71,7 +66,7 @@ SET @DATE = DATEADD(DAY, RAND(CHECKSUM(NEWID())) * (3),
 					)
 				)
 			);
-			*/
+			
 SET @CURRENT_STORAGE_ID = @FINISH_POINT;
 
 INSERT INTO order_history (order_id, status_changed_at, status_id, current_storage_id)
@@ -90,13 +85,3 @@ INSERT INTO order_history (order_id, status_changed_at, status_id, current_stora
 SELECT @ORDER_ID, @DATE, 11, @CURRENT_STORAGE_ID;
 
 
-
-SELECT * FROM order_statuses;
-
-SELECT * FROM storages s
-JOIN storage_types st ON st.id = s.storage_type;
-
-SELECT * FROM orders o
-LEFT JOIN order_history oh ON oh.order_id = o.id
-JOIN products_on_storages ps ON o.product_id = ps.product_id
-WHERE oh.order_id IS NULL;
